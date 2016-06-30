ebf6f6e4b1f9ca55a27f80c4542b3acd5a1f7b00
flyerooo <wxl.pure@gmail.com>
2016-06-30 23:18:32 +0800

    admin配置

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,27 @@
+# coding:utf-8
 from django.contrib import admin
+from models import *
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+	list_display = ('title', 'desc', 'click_count',)
+	list_display_links = ('title', 'desc',)
+	list_editable = ('click_count',)
+
+	fieldsets = (
+		(None, {
+			'fields': ('title', 'desc', 'content',)
+		}),
+		('高级设置', {
+		'classes': ('collapse',),
+		'fields': ('enable_comments', 'registration_required', 'template_name')
+	}),
+		)
+
+admin.site.register(User)
+admin.site.register(Tag)
+admin.site.register(Article)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Links)
+admin.site.register(Ad)
