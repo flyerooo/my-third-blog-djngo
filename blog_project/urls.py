9a458fc1477b542d6f879b1f02245ffd773a5609
flyerooo <wxl.pure@gmail.com>
2016-07-01 12:30:25 +0800

    配置图片上传路径

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
 from blog.views import index
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', index, name='index')
+    url(r'^$', index, name='index'),
+    url(r"^uploads/(?P<path>.*)$", \
+		"django.views.static.serve", \
+		{"document_root": settings.MEDIA_ROOT,}),
 ]
